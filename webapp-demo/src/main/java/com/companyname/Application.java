b5f2d408f61493ff7261df1d53d0995b29a864d8
zeran.wang <wangzeran@outlook.com>
2016-11-14 17:14:53 +0800

    web框架工程增加默认配置文件及资源

--- webapp-demo/src/main/java/com/companyname/Application.java
+++ webapp-demo/src/main/java/com/companyname/Application.java
@@ -1,21 +1,16 @@
 package com.companyname;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 /**
  * 应用入口
  */
 @SpringBootApplication
-@Import(value = {com.companyname.framework.WebConfiguration.class, com.companyname.webapp.config.WebConfiguration.class})
+@Import(value = com.companyname.framework.WebConfiguration.class)
 public class Application {
-
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
-
 }
